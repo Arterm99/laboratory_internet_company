eb1d0ac7a8523ff8f91f7b8beb0b92716fd25140
Artem <loginryabtsev>
2023-01-28 15:58:09 +0300

    project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,18 +19,21 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <!-- Scripts -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Home</a>
+                      <a class="btn btn-success" href="{{ route( 'admin.user.index') }}">К списку
+                                пользователей</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="link text-lg text-gray-700 dark:text-gray-500 underline">Войти</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-lg text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="link ml-4 text-lg text-gray-700 dark:text-gray-500 underline">Регистрация</a>
                         @endif
                     @endauth
                 </div>
